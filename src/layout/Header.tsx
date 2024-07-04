f866880ba2ee5bb098219015ca4a78686cc13da0
AlterShadow <thomas0xken@gmail.com>
2024-07-04 03:35:57 -0400

    update

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,8 +2,9 @@ import { TonConnectButton } from "@tonconnect/ui-react";
 
 const Header = () => {
   return (
-    <div className="w-full">
-      <div className="">
+    <div className="w-full fixed top-0 h-5 bg-white">
+      <div className="flex justify-between items-center px-4 py-3">
+        <div className="text-2xl font-bold">My React Telegram Web App</div>
         <TonConnectButton />
       </div>
     </div>
